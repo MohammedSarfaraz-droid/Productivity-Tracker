ce40d85f800a5c7da4fbe6ea365d534a47f35998
Mohammed Sarfaraz Uddin Nasser <sarfarazuddin9999@gmail.com>
2025-12-20 18:13:33 +0530

    Enhance Todo List and Daily Planner UI with improved structure and styling

--- script.js
+++ script.js
@@ -1,15 +1,31 @@
-var allElems = document.querySelectorAll(".elem");
-var fullElemPage = document.querySelectorAll(".fullElem");
-var fullElemPagebackBtn = document.querySelectorAll(".fullElem .back");
+function openFeatures() {
+  var allElems = document.querySelectorAll(".elem");
+  var fullElemPage = document.querySelectorAll(".fullElem");
+  var fullElemPagebackBtn = document.querySelectorAll(".fullElem .back");
 
-allElems.forEach(function (elem) {
-  elem.addEventListener("click", function () {
-    fullElemPage[elem.id].style.display = "block";
+  allElems.forEach(function (elem) {
+    elem.addEventListener("click", function () {
+      fullElemPage[elem.id].style.display = "block";
+    });
   });
-});
 
-fullElemPagebackBtn.forEach(function (back) {
-  back.addEventListener("click", function () {
-    fullElemPage[back.id].style.display = "none";
+  fullElemPagebackBtn.forEach(function (back) {
+    back.addEventListener("click", function () {
+      fullElemPage[back.id].style.display = "none";
+    });
   });
+}
+openFeatures();
+
+let form = document.querySelector(".addTask form");
+let taskInput = document.querySelector(".addTask form input");
+let taskDetailsInput = document.querySelector(".addTask form textarea");
+let taskCheckbox = document.querySelector(".addTask form #checkbox");
+
+let allTasks = [];
+
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  console.log(taskInput.value, taskDetailsInput.value, taskCheckbox.checked); 
+  
 });
